4ebc862324aad9377acf68cd1a4f865dffc49875
lindabettini <linda.bettini@hotmail.it>
2022-03-17 17:22:51 +0100

    add link home

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
 //Rotte Movies
 Route::get('/movies', 'MovieController@index')->name('movies.index');
